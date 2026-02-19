14581809e4e20a77248061fe4eb469e5c328d22e
Tech-SDS <schneider-stefan@outlook.com>
2026-02-19 21:30:49 +0100

    Fixed routing v4

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/peak-flow/', // Correct base path for GitHub Pages
+  base: command === 'serve' ? '/' : '/peak-flow/', // Root for dev, /peak-flow/ for prod build
   resolve: {
     dedupe: ['leaflet']
   },
@@ -33,4 +33,4 @@ export default defineConfig({
       }
     }
   }
-})
+}))
