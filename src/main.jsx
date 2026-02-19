5b03a0cc1ab0ec24ff1949dd04ac873c45c80127
Tech-SDS <schneider-stefan@outlook.com>
2026-02-19 21:33:31 +0100

    Fixed routing v5

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 import './index.css'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ErrorBoundary>
         <App />
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/peak-flow/', // Root for dev, /peak-flow/ for prod build
+  base: '/', // Root for Vercel deployment
   resolve: {
     dedupe: ['leaflet']
   },
